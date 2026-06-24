249c7d845d591e6e868a14cbacb9b0503cdd2557
Soumya shrivastava <soumyashrivastava2110@gmail.com>
2026-06-24 23:33:42 +0530

    Day-24 update

---
+++ Day-24/LongestWord.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class LongestWord {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+        String[] words = sentence.split(" ");
+        String longest = words[0];
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        System.out.println("Longest word: " + longest);
+        System.out.println("Length: " + longest.length());
+    }
+}
